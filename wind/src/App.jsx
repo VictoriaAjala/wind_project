be6701e0f0d1976d6779db95515789a95b24b3c2
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-03-27 12:26:31 +0100

    react-router-dom

--- wind/src/App.jsx
+++ wind/src/App.jsx
@@ -1,9 +1,43 @@
+import React from "react";
+import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from "./components/Login"
+import Dashboard from "./components/Dashboard";
+
+
 function App() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setLoggedIn(true);
+    }
+
+    const handleLogout = () => {
+        setLoggedIn(false);
+    }
+    
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Windy</h1>
-        </div>
+        <Router>
+            <div className="w-full h-screen">
+                <Routes>
+                    <Route exact path='/' element={ loggedIn ? ( <Navigate to='/dashboard' /> 
+                    ) : (
+                        <Login onLogin={handleLogin} />
+                    )
+                }
+                />
+                <Route path="dashboard" element={
+                    loggedIn ? (
+                        <Dashboard />
+                    ) : (
+                        <Navigate to='/' />
+                    )
+                }
+                />
+                </Routes>
+            </div>
+        </Router>
     )
 }
 
-export default App
+export default App;
