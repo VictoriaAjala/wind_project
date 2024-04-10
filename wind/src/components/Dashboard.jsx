f4ea7e9519588ade5b423cd1ce7b6727fe0ceb19
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-04-10 13:12:24 +0100

    layout to dashboard-comp

--- wind/src/components/Dashboard.jsx
+++ wind/src/components/Dashboard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Layout from './shared/Layout'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Layout />
+    </div>
   )
 }
