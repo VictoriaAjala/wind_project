78da8464af9f1b4e91355a91f9e2055f7ed3e1e9
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-05-09 11:36:26 +0100

    routes and icons edit

--- wind/src/components/shared/Header.jsx
+++ wind/src/components/shared/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FiSearch } from 'react-icons/fi'
+import { FiSearch, FiBell } from 'react-icons/fi'
+import { BiMessageDots } from "react-icons/bi";
 
 export default function Header() {
   return (
@@ -10,6 +11,10 @@ export default function Header() {
             placeholder='Search for anything here'
             className='text-base focus:outline-none active:outline-none h-10 w-[26rem] border-[1.5px] border-gray-400 pl-4 pr-11 shadow-sm rounded-lg' />
             <FiSearch fontSize={25} className='text-gray-400 absolute top-1/2 -translate-y-1/2 right-8' />
+        </div>
+        <div className='flex items-center gap-2 mr-2'>
+          <BiMessageDots fontSize={24} />
+          <FiBell fontSize={24} />
         </div>
     </div>
   )
