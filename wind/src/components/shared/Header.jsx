ec3bd83946bf742d9016c541dda73832305d9f81
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-05-10 17:56:46 +0100

    messages comp

--- wind/src/components/shared/Header.jsx
+++ wind/src/components/shared/Header.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { FiSearch, FiBell } from 'react-icons/fi'
 import { BiMessageDots } from "react-icons/bi";
+import { Popover, Transition } from '@headlessui/react'
+import classNames from 'classnames';
 
 export default function Header() {
   return (
@@ -13,7 +15,34 @@ export default function Header() {
             <FiSearch fontSize={25} className='text-gray-400 absolute top-1/2 -translate-y-1/2 right-8' />
         </div>
         <div className='flex items-center gap-2 mr-2'>
-          <BiMessageDots fontSize={24} />
+          <Popover className="relative">
+            {({ open }) => (
+              <>
+                  <Popover.Button
+                    className={classNames(
+                      open && 'bg-gray-100',
+                      "p-1.5 rounded-sm inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100")}>
+                      <BiMessageDots fontSize={24} />
+                    </Popover.Button>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                      >
+                        <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                          <div className='bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5'>
+                            <strong className='text-gray-700 font-medium'>Messages</strong>
+                            <div className='nt-2 py-1 text-sm'>This is massages panel.</div>
+                          </div>
+                        </Popover.Panel>
+                      </Transition>
+              </>
+            )}
+          </Popover>
           <FiBell fontSize={24} />
         </div>
     </div>
