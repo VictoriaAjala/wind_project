dfd1b9a9ba5ea02c53e56638777f62e998e7d8f0
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-05-15 19:50:00 +0100

    statsgrid

---
+++ wind/src/components/shared/DashboardStatsGrid.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function DashboardStatsGrid() {
+  return (
+    <div className="flx gap-4">
+        <div className='bg-white'></div>
+        <div className='bg-white'></div>
+        <div className='bg-white'></div>
+        <div className='bg-white'></div>
+    </div>
+  )
+}
