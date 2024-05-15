86168e7e1d7a9a00b33736891a3b0ff1aa5036f1
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-05-15 19:48:55 +0100

    widocx

--- wind/src/components/Dashboard.jsx
+++ wind/src/components/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './shared/Layout'
+import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
   return (
     <div>
-      <Layout />
+      <p>this is dashboard</p>
+      <Link to="products" className='underline'>
+        products
+      </Link>
     </div>
   )
 }
