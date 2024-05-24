632a836c3dee70e3a51d07c101ef7cad8e97f9dc
VictoriaAjala <victoria.o.ajala@gmail.com>
2024-05-24 09:24:17 +0100

    tsting

--- wind/src/components/Dashboard.jsx
+++ wind/src/components/Dashboard.jsx
@@ -5,8 +5,9 @@ import DashboardStatsGrid from './DashboardStatsGrid'
 
 export default function Dashboard() {
   return (
-      <div className='flex gap-4'>
-        <DashboardStatsGrid />
+    <div>
+            <div className='flex flex-col gap-4'>
       </div>
+    </div>
   )
 }
